ddcb9ee66b0598134c6f78c8a9afac0a52e17a85
taehee <gmglgmgn@naver.com>
2025-09-16 19:41:22 +0900

    refactor(front/logo): 로고 예시 박스가 실제 이미지 예시를 보이도록 수정

--- frontend/src/atoms/LogoExampleBox/LogoExampleBox.tsx
+++ frontend/src/atoms/LogoExampleBox/LogoExampleBox.tsx
@@ -3,6 +3,10 @@
 // ---------------------------------
 import type { LogoType } from '../../types/logoTypes';
 import styles from './LogoExampleBox.module.css';
+import  OnlyImage  from '../../assets/images/logo_types/OnlyImage.png';
+import { Image } from '../Image/Image';
+import Text from '../../assets/images/logo_types/Text.png';
+import ImageAndText from '../../assets/images/logo_types/ImageAndText.png';
 
 export type LogoExampleBoxProps = {
     type: LogoType;
@@ -14,14 +18,16 @@ export function LogoExampleBox ({ type } : LogoExampleBoxProps) {
         <div className={styles.box} aria-label={`예시: ${type}`}>
             {type === 'TEXT' && (
                 <div className={styles.centerCol}>
-                    <div className={styles.bigWord}>LOGO</div>
+                    {/* <div className={styles.bigWord}>LOGO</div> */}
+                    <Image src={Text}/>
                     <div className={styles.caption}>타입 예시</div>
                 </div>
             )}
 
             {type === 'ICON' && (
                 <div className={styles.centerCol}>
-                    <div className={styles.iconCircle} />
+                    {/* <div className={styles.iconCircle} /> */}
+                    <Image src={OnlyImage}></Image>
                     <div className={styles.caption}>그림만 있는 로고</div>
                 </div>
             )}
@@ -29,8 +35,9 @@ export function LogoExampleBox ({ type } : LogoExampleBoxProps) {
             {type === 'COMBO' && (
                 <div className={styles.centerCol}>
                     <div className={styles.comboRow}>
-                        <div className={styles.iconSquare} />
-                        <div className={styles.comboText}>COMPANY LOGO</div>
+                        {/* <div className={styles.iconSquare} /> */}
+                        {/* <div className={styles.comboText}>COMPANY LOGO</div> */}
+                        <Image src={ImageAndText}/>                    
                     </div>
                     <div className={styles.caption}>그림과 텍스트가 함께 있는 로고</div>
                 </div>
