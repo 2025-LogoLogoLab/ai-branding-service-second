9619cfc53c5d0a4e114504a26d52d4a67f03e50c
taehee <gmglgmgn@naver.com>
2025-09-29 16:03:14 +0900

    feat(logo): 백 엔드 로고 생성 요청 시 로고 type을 전달 하도록 추가

--- frontend/src/atoms/TextButton/TextButton.tsx
+++ frontend/src/atoms/TextButton/TextButton.tsx
@@ -5,7 +5,7 @@ import styles from './TextButton.module.css';
 export type ButtonProps = {
     label: string;        // 버튼 내용 
     onClick: () => void;    // 버튼 클릭 시 실행할 동작
-    variant?: 'white' | 'orange' | 'outlined' | 'card';   // Text 버튼 종류 지정 (CSS 클래스 지정용)
+    variant?: 'white' | 'orange' | 'outlined' | 'card' | 'blue';   // Text 버튼 종류 지정 (CSS 클래스 지정용)
 };
 
 export function TextButton( { label, onClick, variant = 'white' } : ButtonProps ) {
