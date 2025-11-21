336ac6ac23d0e3f68b3b2ce0cc3629d60782a388
taehee <gmglgmgn@naver.com>
2025-11-21 19:31:43 +0900

    refactor(front/UI): 전역 푸터 리뉴얼

--- frontend/src/organisms/layout/GlobalFooter/GlobalFooter.tsx
+++ frontend/src/organisms/layout/GlobalFooter/GlobalFooter.tsx
@@ -1,6 +1,11 @@
 // src/organisms/layout/GlobalFooter/GlobalFooter.tsx
 import { Link } from "react-router-dom";
+import type { MouseEvent } from "react";
 import styles from "./GlobalFooter.module.css";
+import brandImage from "../../../assets/designs/LogoLogoLabHeader.png";
+import facebookIcon from "../../../assets/icons/icon_Facebook.png";
+import instagramIcon from "../../../assets/icons/icon_Instagram.png";
+import twitterIcon from "../../../assets/icons/icon_Twitter.png";
 
 /**
  * GlobalFooter
@@ -9,45 +14,59 @@ import styles from "./GlobalFooter.module.css";
  * - 실제 링크 URL은 추후 정책/페이지에 맞게 연결
  */
 export default function GlobalFooter() {
+  const handleNotImplemented = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    alert("아직 구현되지 않은 기능입니다.");
+  };
+
   return (
     <div className={styles.wrap} role="contentinfo">
       <div className={styles.topRow}>
         <div className={styles.brandCol}>
-          <Link to="/" className={styles.brand} aria-label="LogoLogoLab home">
-            <span className={styles.spark}>✷</span>
-            <span className={styles.brandText}>LogoLogoLab</span>
+          <Link
+            to="/"
+            className={styles.brand}
+            aria-label="LogoLogoLab home"
+            onClick={handleNotImplemented}
+          >
+            <img src={brandImage} alt="LogoLogoLab" className={styles.brandImage} />
           </Link>
           <div className={styles.socials}>
-            {/* 실제 아이콘 자산이 생기면 교체 */}
-            <a href="#" aria-label="Instagram">Instagram</a>
-            <a href="#" aria-label="Twitter">Twitter</a>
-            <a href="#" aria-label="LinkedIn">LinkedIn</a>
+            <a href="#" aria-label="Facebook" className={styles.socialLink} onClick={handleNotImplemented}>
+              <img src={facebookIcon} alt="" className={styles.socialIcon} />
+            </a>
+            <a href="#" aria-label="Instagram" className={styles.socialLink} onClick={handleNotImplemented}>
+              <img src={instagramIcon} alt="" className={styles.socialIcon} />
+            </a>
+            <a href="#" aria-label="Twitter" className={styles.socialLink} onClick={handleNotImplemented}>
+              <img src={twitterIcon} alt="" className={styles.socialIcon} />
+            </a>
           </div>
         </div>
 
         <div className={styles.linksGrid}>
           <div>
             <div className={styles.head}>Company</div>
-            <a href="#">About Us</a>
-            <a href="#">Careers</a>
-            <a href="#">Press</a>
+            <a href="#" onClick={handleNotImplemented}>About Us</a>
+            <a href="#" onClick={handleNotImplemented}>Careers</a>
+            <a href="#" onClick={handleNotImplemented}>Press</a>
           </div>
           <div>
             <div className={styles.head}>Resources</div>
-            <a href="#">Blog</a>
-            <a href="#">Help Center</a>
-            <a href="#">Contact Support</a>
+            <a href="#" onClick={handleNotImplemented}>Blog</a>
+            <a href="#" onClick={handleNotImplemented}>Help Center</a>
+            <a href="#" onClick={handleNotImplemented}>Contact Support</a>
           </div>
             <div>
               <div className={styles.head}>Legal</div>
-              <a href="#">Privacy Policy</a>
-              <a href="#">Terms of Service</a>
+              <a href="#" onClick={handleNotImplemented}>Privacy Policy</a>
+              <a href="#" onClick={handleNotImplemented}>Terms of Service</a>
             </div>
             <div>
               <div className={styles.head}>Follow Us</div>
-              <a href="#">Instagram</a>
-              <a href="#">Twitter</a>
-              <a href="#">LinkedIn</a>
+              <a href="#" onClick={handleNotImplemented}>Instagram</a>
+              <a href="#" onClick={handleNotImplemented}>Twitter</a>
+              <a href="#" onClick={handleNotImplemented}>LinkedIn</a>
             </div>
         </div>
       </div>
