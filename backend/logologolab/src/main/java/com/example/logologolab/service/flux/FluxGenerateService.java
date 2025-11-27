4c2a32a31bde6c2fc9f9ccde72cdf639194cc0ad
Dongwook Kim <dongwook258@naver.com>
2025-11-27 09:49:22 +0900

    refactor(backend) : 런팟 주소 변경

--- backend/logologolab/src/main/java/com/example/logologolab/service/flux/FluxGenerateService.java
+++ backend/logologolab/src/main/java/com/example/logologolab/service/flux/FluxGenerateService.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 @Service
 public class FluxGenerateService {
 
-    private static final String AI_SERVER_URL = "http://195.26.233.76:39535/generate-logo";
+    private static final String AI_SERVER_URL = "http://213.181.122.175:13991/generate-logo";
 
     private static final Set<String> ALLOWED_STYLES = Set.of(
             "simple","minimal","retro","vintage","cute","playful","luxury",
